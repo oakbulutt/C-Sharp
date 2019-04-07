08dffab7261820a8e77c2f28e8e95e1a39ac3127
Oguzhan Akbulut <oakbulutt@outlook.com>
2019-04-07 19:26:29 +0200

    BP6Exercise3 - Increasing Sums

---
+++ BP6Exercise3/BP6Exercise3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BP6Exercise3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number for value of N:");
+            int n = int.Parse(Console.ReadLine());
+            int sum = 1;
+
+            if (n >= 1)
+            {
+                for (int i = 1; i <= n; i++)
+                {
+                    Console.WriteLine($"{i}: {sum}");
+                    sum = sum + i + 1;
+                }
+            }
+            else
+                return;
+
+            Console.WriteLine("Press any key to exit..!");
+            Console.ReadKey();
+        }
+    }
+}
