497d8f3d6e33813eff2a92a15930ca1811f1511f
Oguzhan Akbulut <oakbulutt@outlook.com>
2019-04-07 19:24:15 +0200

    BP5Exercise1 - A Sum of Numbers

---
+++ BP5Exercise1/BP5Exercise1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BP5Exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum=0;
+            for (int i = 1; i <= 100; i++)
+            {
+                sum += i;
+            }
+            Console.WriteLine("For loop: "+sum);
+
+            int a = 1;
+            int result = 0;
+            while (a <= 100)
+            {
+                result += a;
+                a++;
+            }
+            Console.WriteLine("While loop: "+result);
+
+            Console.WriteLine("Press any key to exit..!");
+            Console.ReadKey();
+        }
+    }
+}
