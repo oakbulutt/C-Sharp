6340fa4f62382a863cbea722034adbf0e0d93e45
Oguzhan Akbulut <oakbulutt@outlook.com>
2019-04-07 19:25:56 +0200

    BP6Exercise2 - Calculation of An Arithmetic Mean

---
+++ BP6Exercise2/BP6Exercise2/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BP6Exercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList num = new ArrayList();
+            double arithmeticMean = 0;
+            double total = 0;
+            while (true)
+            {
+                Console.WriteLine($"Enter number:");
+                string tempNum = Console.ReadLine();
+
+                if (IsEmpty(tempNum) == true && IsNum(tempNum) == true)
+                {
+                    num.Add(Convert.ToDouble(tempNum));
+                }
+                else
+                    break;
+            }
+            foreach(double elements in num)
+            {
+                total += elements;
+                arithmeticMean = total / num.Count;
+            }
+
+            Console.WriteLine("An arithmetic mean of the numbers is: " + arithmeticMean);
+
+            Console.WriteLine("Press any key to exit..!");
+            Console.ReadKey();
+
+        }
+        static bool IsEmpty(string x)
+        {
+            if (x == "")
+                return false;
+            else
+                return true;
+        }
+        static bool IsNum(string x)
+        {
+            try
+            {
+                Convert.ToDouble(x);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
