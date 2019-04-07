6024f9949e87378596e722489e97eb3845782882
Oguzhan Akbulut <oakbulutt@outlook.com>
2019-04-07 18:35:12 +0200

    BP2Exercise4 - Recursion: Factorial Calculation

---
+++ BP2Exercise4/BP2Exercise4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BP2Exercise4
+{
+    class Program
+    {
+        static int factorial(int n)
+        {
+            if(n == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return n = n * factorial(n - 1);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter number for factorial calculation:");
+            int input = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(factorial(input));
+            Console.WriteLine("Press any key to exit..!");
+            Console.ReadKey();
+        }
+    }
+}
