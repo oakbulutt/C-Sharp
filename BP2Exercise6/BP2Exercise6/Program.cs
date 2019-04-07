2a3ac1c2f8d595a2e479a922c63697e0a9612ea0
Oguzhan Akbulut <oakbulutt@outlook.com>
2019-04-07 18:38:18 +0200

    BP2Exercise6 - Recursion: Greatest common divisor

---
+++ BP2Exercise6/BP2Exercise6/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BP2Exercise6
+{
+    class Program
+    {
+        static int GDC(int n, int k)
+        {
+            if (k == 0) return n;
+            int remaining = n % k;
+            if (remaining == 0) return k;
+            return GDC(k, remaining);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter firs number:");
+            int firstNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter second number:");
+            int secondNumber = int.Parse(Console.ReadLine());
+            if (firstNumber <= secondNumber)
+            {
+                Console.WriteLine($"Greatest Common Diviser of {firstNumber} and {secondNumber} numbers: " +
+                    $"{GDC(firstNumber, secondNumber)}\nPress any key to exit..!");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Invalid operation..!\nPress any key to exit..!");
+                Console.ReadKey();
+            }
+        }
+    }
+}
