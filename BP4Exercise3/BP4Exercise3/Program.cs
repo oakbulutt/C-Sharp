4b3981716aa6db0b4a2ad473e8724438c6129e7b
Oguzhan Akbulut <oakbulutt@outlook.com>
2019-04-07 19:23:43 +0200

    BP4Exercise3 - Rounding Towards Zero

---
+++ BP4Exercise3/BP4Exercise3/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BP4Exercise3
+{
+    class Program
+    {
+        static double RoundTowardsZero(double x)
+        {
+            double result = 0;
+            if ((Math.Ceiling(x) - x) > 0.5)
+            {
+                result = Math.Floor(x);
+                return result;
+            }
+            else if((Math.Ceiling(x) - x) <= 0.5)
+            {
+                result = Math.Ceiling(x);
+                return result;
+            }
+            else
+                return result;
+        }
+        static void Main(string[] args)
+        {
+            double a = 5.23;
+            double b = 5.67;
+
+            Console.WriteLine($"Round Towards Zero:\n" +
+                $"BEFORE:\t AFTER:\n" +
+                $"{a}\t {RoundTowardsZero(a)}");
+
+            Console.WriteLine($"Round Towards Zero:\n" +
+                $"BEFORE:\t AFTER:\n" +
+                $"{b}\t {RoundTowardsZero(b)}");
+
+            Console.WriteLine("Press any key to exit..!");
+            Console.ReadKey();
+
+            
+        }
+    }
+}
