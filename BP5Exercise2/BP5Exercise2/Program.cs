95b5af6e611910c394baa2e0740495b0e15baf1d
Oguzhan Akbulut <oakbulutt@outlook.com>
2019-04-07 19:24:52 +0200

    BP5Exercise2 - A Sum of Even Numbers

---
+++ BP5Exercise2/BP5Exercise2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BP5Exercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            for (int i = 0; i <= 100; i += 2)
+            {
+                sum += i;
+            }
+            Console.WriteLine(sum);
+
+            Console.WriteLine("Press any key to exit..!");
+            Console.ReadKey();
+        }
+    }
+}
