e91275b7b5f931386c241cc4d794ffff91ee6d31
Oguzhan Akbulut <oakbulutt@outlook.com>
2019-04-07 19:27:03 +0200

    BP6Exercise4 - A Square Pattern

---
+++ BP6Exercise4/BP6Exercise4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BP6Exercise4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number for value of N:");
+            int n = int.Parse(Console.ReadLine());
+            for (int row = 1; row <= n; row++)
+            {
+                for (int col = 1; col <= row; col++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+            }
+            Console.WriteLine("Press any key to exit..!");
+            Console.ReadKey();
+        }
+    }
+}
