deeedb49216bc0b8b39b87832293636ac31bc7db
Oguzhan Akbulut <oakbulutt@outlook.com>
2019-05-31 13:57:38 +0200

    BP13Exercise3

---
+++ BP13Exercise3/BP13Exercise3/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BP13Exercise3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+            double x;
+
+            if(double.TryParse(textBox1.Text, out x))
+            {
+                label1.Text = (x * x).ToString();
+                label2.Text = (x * x * x).ToString();
+                label3.Text = (x * x * x * x).ToString();
+            }
+            else
+            {
+                label1.Text = "-";
+                label2.Text = "-";
+                label3.Text = "-";
+            }
+        }
+    }
+}
