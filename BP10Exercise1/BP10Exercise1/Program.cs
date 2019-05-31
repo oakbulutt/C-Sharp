3c61cb4fc50b9e88a731740bc7990209b7478c2f
Oguzhan Akbulut <oakbulutt@outlook.com>
2019-05-31 13:41:57 +0200

    BP10Exercise1

---
+++ BP10Exercise1/BP10Exercise1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace BP10Exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number which you want to convert to binary.");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(findBinary(num));
+
+            Console.WriteLine("Press any key to exit..!");
+            Console.ReadKey();
+        }
+        static int findBinary(int number)
+        {
+            if (number == 0)
+                return 0;
+            else
+                return (number % 2 + 10 * findBinary(number / 2));
+        }
+    }
+}
